6bf5b7b5c986a9f1202a41e344842604889d7c46
jerihosterr01 <162899370+jerihosterr01@users.noreply.github.com>
2024-03-10 23:29:53 +0700

    ezy.js

---
+++ ezy.js
@@ -0,0 +1,6 @@
+<script>
+if (!location.hostname.endsWith("gfa.my.id")) {
+  alert("Created By Jerihost," + " Jangan Maling Mulu Kontol Modal Kontol!!!!");
+  location.href = "about:blank";
+}
+</script>
