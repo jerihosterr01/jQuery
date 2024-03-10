e779ba56e34e7ae9fb5d00f6c9d24363ac7fb467
jerihosterr01 <162899370+jerihosterr01@users.noreply.github.com>
2024-03-10 22:40:58 +0700

    verif.js

--- verif.js
+++ verif.js
@@ -1,8 +1,5 @@
-// ALERT NO LINK
-if (window.location.origin.indexOf("gfa.my.id") < 0) {
-    alert("Peringatan!!\n\nInformasi IP anda telah terkirim");
-    alert("Create By Jerihosting");
-    location.href = "about:blank";
-    document.body.innerHTML = "";
-    return true;
+// ALERT 2X
+if (document.domain !== "gfa.my.id") {
+  alert("Created By Jerihoster" + " Jangan Nyolong Mulu Modal Donk!!!!");
+  location.href = "about:blank";
 }
